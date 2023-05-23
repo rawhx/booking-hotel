e87b1fc3dca9301934344bb5a7d087556bd56b7e
rawhx_x <achmadhasbil24@gmail.com>
2023-05-23 14:41:32 +0700

    anu

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['guest'])->group(function () {
-    // Route::get('/', function (){
-    //    return ('hi');
-    // });
     Route::get('/', [AuthContoroller::class, 'index'])->name('home');
     Route::post('/', [AuthContoroller::class, 'login'])->name('login');
 });
